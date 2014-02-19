04499924b12516e86fcff65f6398ee173e8ca566
Raqib Hakim <raqibhakim89@gmail.com>
2014-02-19 08:50:28 +0800

    Update E Filing | Campus Events Index Page

--- db/schema.rb
+++ db/schema.rb
@@ -123,13 +123,6 @@ ActiveRecord::Schema.define(version: 20140213070431) do
     t.datetime "updated_at"
   end
 
-  create_table "articles", force: true do |t|
-    t.string   "name"
-    t.text     "content"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "asset_defects", force: true do |t|
     t.integer  "asset_id"
     t.integer  "reported_by"
