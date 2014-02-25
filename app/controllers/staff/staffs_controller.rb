0a1f505baf290e50b407afbb9714071931c8c818
maxcobmara <maxcobmara@gmail.com>
2014-02-26 04:18:54 +0800

    staff edit page started

--- app/controllers/staff/staffs_controller.rb
+++ app/controllers/staff/staffs_controller.rb
@@ -8,9 +8,7 @@ class Staff::StaffsController < ApplicationController
     @search = Staff.search(params[:q])
     @staffs = @search.result
     @staffs = @staffs.page(params[:page]||1)
-    #previous
-    #@staff_filtered = Staff.with_permissions_to(:edit).find(:all, :order => sort_column + ' ' + sort_direction ,:conditions => ['icno LIKE ? or name ILIKE ?', "%#{params[:search]}%", "%#{params[:search]}%"])
-    @staff_filtered = Staff.find(:all, :order => sort_column + ' ' + sort_direction ,:conditions => ['icno LIKE ? or name ILIKE ?', "%#{params[:search]}%", "%#{params[:search]}%"])
+    @infos = @staffs
   end
 
   # GET /staffs/1
@@ -48,7 +46,7 @@ class Staff::StaffsController < ApplicationController
   def update
     respond_to do |format|
       if @staff.update(staff_params)
-        format.html { redirect_to @staff, notice: 'Staff was successfully updated.' }
+        format.html { redirect_to staff_info_path, notice: 'Staff was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
@@ -71,17 +69,11 @@ class Staff::StaffsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_staff
       @staff = Staff.find(params[:id])
+      @info = @staff
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def staff_params
-      params.require(:staff).permit(:icno, :rank_id, :name)
-    end
-    
-    def sort_column
-        Staff.column_names.include?(params[:sort]) ? params[:sort] : "icno" 
-    end
-    def sort_direction
-        %w[asc desc].include?(params[:direction])? params[:direction] : "asc" 
+      params.require(:staff).permit(:icno, :name, :code, :fileno, :coemail, :cobirthdt, :thumb_id)
     end
 end
