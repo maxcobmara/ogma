30ef48aa546e0cf611a3964367a6961d0146d86b
diaarezz <your.akuarezz@gmail.com>
2014-03-13 17:21:41 +0800

    update show page/student info

--- app/models/qualification.rb
+++ app/models/qualification.rb
@@ -1,18 +1,26 @@
 class Qualification < ActiveRecord::Base
   belongs_to :staff
-end
+    
+  QTYPE = [
+       #  Displayed       stored in db
+       [ "Sekolah Rendah", 17 ],
+       [ "PMR/SRP dan setaraf", 16],
+       [ "SPM/SPVM dan setaraf", 15 ],
+       [ "Senior Cambridge Cert", 14 ],
+       [ "STPM dan setaraf", 13 ],
+       [ "Sijil", 12 ],
+       [ "Diploma", 11 ],
+       [ "Diploma Lanjutan", 10 ],
+       [ "Diploma Lepasan Ijazah", 9 ],
+       [ "Sarjana Muda", 8 ],
+       [ "Sarjana Muda Kepujian", 7 ],
+       [ "Sarjana Persuratan", 6 ],
+       [ "Sarjana Sastera", 5 ],
+       [ "Sarjana Sains", 4 ],
+       [ "Sarjana Perubatan", 3 ],
+       [ "Lepasan Sarjana",2 ],
+       [ "Doktor Falsafah (Ph.D)",1 ],
+       [ "Lain Lain", 99 ]
+  ]
 
-# == Schema Information
-#
-# Table name: qualifications
-#
-#  created_at   :datetime
-#  id           :integer          not null, primary key
-#  institute    :string(255)
-#  institute_id :integer
-#  level_id     :integer
-#  qname        :string(255)
-#  staff_id     :integer
-#  student_id   :integer
-#  updated_at   :datetime
-#
+end
