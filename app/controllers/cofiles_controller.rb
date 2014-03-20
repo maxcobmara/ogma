7063635e610e3ae3c2cb00dd00e80a477669fef3
diaarezz <your.akuarezz@gmail.com>
2014-03-20 17:17:54 +0800

    update file registry and student info module

--- app/controllers/cofiles_controller.rb
+++ app/controllers/cofiles_controller.rb
@@ -1,5 +1,4 @@
 class CofilesController < ApplicationController
-  helper_method :sort_column, :sort_direction
   before_action :set_cofile, only: [:show, :edit, :update, :destroy]
   # GET /cofiles
   # GET /cofiles.xml
@@ -10,7 +9,7 @@ class CofilesController < ApplicationController
     @cofiles = @cofiles.page(params[:page]||1)
     #previous
     #@staff_filtered = Staff.with_permissions_to(:edit).find(:all, :order => sort_column + ' ' + sort_direction ,:conditions => ['icno LIKE ? or name ILIKE ?', "%#{params[:search]}%", "%#{params[:search]}%"])
-    @cofiles_filtered = Cofile.find(:all, :order => sort_column + ' ' + sort_direction ,:conditions => ['cofileno LIKE ? or name ILIKE ? ', "%#{params[:search]}%", "%#{params[:search]}%"])
+    #@cofiles_filtered = Cofile.find(:all, :order => sort_column + ' ' + sort_direction ,:conditions => ['cofileno LIKE ? or name ILIKE ? ', "%#{params[:search]}%", "%#{params[:search]}%"])
 
   end
   
@@ -21,7 +20,7 @@ class CofilesController < ApplicationController
   def update
     respond_to do |format|
       if @cofile.update(cofile_params)
-        format.html { redirect_to cofile_path, notice: 'File Registry was successfully updated.' }
+        format.html { redirect_to @cofile, notice: 'File Registry was successfully updated.' }
         format.json { head :no_content }
       else
         format.html { render action: 'edit' }
@@ -29,6 +28,7 @@ class CofilesController < ApplicationController
       end
     end
   end
+
   
   def edit
     @cofile = Cofile.find(params[:id])
@@ -70,11 +70,5 @@ private
     def cofile_params
       params.require(:cofile).permit(:cofileno, :name, :location, :owner_id, :staffloan_id, :onloandt, :onloanxdt)
     end
-    
-    def sort_column
-        Cofile.column_names.include?(params[:sort]) ? params[:sort] : "cofileno" 
-    end
-    def sort_direction
-        %w[asc desc].include?(params[:direction])? params[:direction] : "asc" 
-    end
+
 end
