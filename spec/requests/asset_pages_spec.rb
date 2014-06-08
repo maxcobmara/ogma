304a0b7fc013db5cb018e59ab20b391f8739d2b8
MaxCobMara <maxcobmara@gmail.com>
2014-06-09 01:16:57 +0800

    rspec for kewpa links

--- spec/requests/asset_pages_spec.rb
+++ spec/requests/asset_pages_spec.rb
@@ -10,6 +10,9 @@ describe "asset pages" do
     before { visit asset_assets_path }
     
     it { should have_selector('h1', text: 'Assets') }
+    it { should have_link("", href: kewpa4_asset_assets_path(:format => 'pdf') + "?locale=en")}
+    it { should have_link("", href: kewpa5_asset_assets_path(:format => 'pdf') + "?locale=en")}
+    it { should have_link("", href: kewpa13_asset_assets_path(:format => 'pdf') + "?locale=en")}
     it { should have_selector('th', text: 'Registration Serial No') }
     it { should have_selector('th', text: 'Manufacturer/Brand') }
     it { should have_selector('th', text: 'Local Order Date (LO Date)')}
@@ -30,8 +33,10 @@ describe "asset pages" do
     it { should have_link("", href: asset_assets_path + "?locale=en")}
     it { should have_link("", href: kewpa2_asset_asset_path(@asset, format: "pdf") + "?locale=en")}
     it { should_not have_link("", href: kewpa3_asset_asset_path(@asset, format: "pdf") + "?locale=en")}
+    it { should have_link("", href: kewpa6_asset_asset_path(@asset, format: "pdf") + "?locale=en")}
     it { should have_link("", href: new_asset_defect_path(:asset_id => @asset) + "&locale=en")}
     it { should have_link("", href: edit_asset_asset_path(@asset) + "?locale=en")}
+    it { should have_link("", href: asset_asset_path(@asset)+ "?locale=en")}
   end
   
   describe "Inventory Show Page" do
@@ -46,6 +51,10 @@ describe "asset pages" do
     it { should have_link("", href: asset_assets_path + "?locale=en")}
     it { should_not have_link("", href: kewpa2_asset_asset_path(@inventory, format: "pdf") + "?locale=en")}
     it { should have_link("", href: kewpa3_asset_asset_path(@inventory, format: "pdf") + "?locale=en")}
+    it { should have_link("", href: kewpa6_asset_asset_path(@inventory, format: "pdf") + "?locale=en")}
+    it { should_not have_link("", href: new_asset_defect_path(:asset_id => @inventory) + "&locale=en")}
+    it { should have_link("", href: edit_asset_asset_path(@inventory) + "?locale=en")}
+    it { should have_link("", href: asset_asset_path(@inventory)+ "?locale=en")}
   end
   
   describe "Asset Edit Page" do
