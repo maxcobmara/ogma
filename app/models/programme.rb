9568f6bb14b38428e0e35c3ac75954866ca9ac61
Nurhashimah <nurhashimah77@gmail.com>
2014-06-24 22:24:19 +0800

    Weeklytimetable - EDIT page (works for Existing record only, pending on non-assigned timeslot[during New/record creation])

--- app/models/programme.rb
+++ app/models/programme.rb
@@ -12,6 +12,14 @@ class Programme < ActiveRecord::Base
     end
   end
   
+  def semester_subject_topic
+    if ancestry_depth == 3
+      "Sem #{parent.parent.code}"+"-"+"#{parent.code}"+" | "+"#{name}"
+    elsif ancestry_depth == 4
+      ">>Sem #{parent.parent.parent.code}"+"-"+"#{parent.parent.code}"+" | "+"#{code} "+"#{name}"
+    end
+  end
+  
 end
 
 # == Schema Information
