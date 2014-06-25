fd9d7ff00bd7edcf48d4fb6807e89ca918db65c2
irshad89 <isyaduddin@gmail.com>
2014-06-25 16:03:04 +0800

    print by year

--- config/routes.rb
+++ config/routes.rb
@@ -157,6 +157,7 @@ Ogma::Application.routes.draw do
   match '/contact', to: 'static_pages#contact', via: 'get'
   match '/dashboard', to: 'static_pages#dashboard', via: 'get'
   match '/asset_report', to: 'static_pages#asset_report', via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
