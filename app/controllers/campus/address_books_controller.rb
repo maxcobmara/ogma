54453187135152b261a8f37f3ddf20a3a303e532
Yaacob Noor <maxcobmara@gmail.com>
2014-07-14 21:52:50 +0800

    create contact

--- app/controllers/campus/address_books_controller.rb
+++ app/controllers/campus/address_books_controller.rb
@@ -1,4 +1,6 @@
 class Campus::AddressBooksController < ApplicationController
+
+  before_action :set_address_book, only: [:show, :edit, :update, :destroy]
   
   def index
     @address_books = AddressBook.where(name: params[:search])
@@ -7,4 +9,36 @@ class Campus::AddressBooksController < ApplicationController
   def new
   	@address_book = AddressBook.new
   end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def create 
+  	@address_book = AddressBook.new(address_book_params)
+
+    respond_to do |format|
+      if @address_book.save
+        format.html { redirect_to campus_address_books_path, notice: 'Contact was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @address_book }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @address_book.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+
+	private
+	  # Use callbacks to share common setup or constraints between actions.
+	  def set_address_book
+	    @address_book = AddressBook.find(params[:id])
+	  end
+
+	  # Never trust parameters from the scary internet, only allow the white list through.
+	  def address_book_params
+	    params.require(:address_book).permit(:address, :fax, :mail, :name, :phone, :shortname, :web)
+	  end
 end
