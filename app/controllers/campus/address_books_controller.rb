48d33834e899750902be972c9df2f75989bcb56f
Yaacob Noor <maxcobmara@gmail.com>
2014-07-14 09:50:40 +0800

    addressbook updates

--- app/controllers/campus/address_books_controller.rb
+++ app/controllers/campus/address_books_controller.rb
@@ -3,4 +3,8 @@ class Campus::AddressBooksController < ApplicationController
   def index
     @address_books = AddressBook.where(name: params[:search])
   end
+
+  def new
+  	@address_book = AddressBook.new
+  end
 end
