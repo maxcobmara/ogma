da74d467f89ce1dfe4080ee6d70ec738b88ffa6d
irshad89 <isyaduddin@gmail.com>
2014-07-16 15:11:41 +0800

    bold

--- app/pdfs/kewpa19_pdf.rb
+++ app/pdfs/kewpa19_pdf.rb
@@ -11,31 +11,31 @@ class Kewpa19Pdf < Prawn::Document
     move_down 20
     text "SIJIL PELUPUSAN ASET KERAJAAN MALAYSIA", :align => :center, :size => 14, :style => :bold
     move_down 20
-    text "Merujuk surat kelulusan No rujukan   #{@disposal.document.refno}  , bertarikh   #{@disposal.document.letterdt} , 
-    saya mengesahkan tindakan pelupusan telah dilaksanakan seperti berikut :-", :align => :left, :size => 10
+    text "Merujuk surat kelulusan No rujukan   <b>#{@disposal.document.refno}</b>  , bertarikh   <b>#{@disposal.document.letterdt}</b> , 
+    saya mengesahkan tindakan pelupusan telah dilaksanakan seperti berikut :-", :align => :left, :size => 10, :inline_format => true
     move_down 10
     text "1.  Aset berikut telah dilupuskan secara pindahan/hadiah." , :align => :left, :width => 200, :size => 10
-    text "  Bilangan item #{@disposal.quantity} dipindahkan / hadiah kepada #{@disposal.receiver_name} ", :align => :left, :width => 200, :size => 10, :indent_paragraphs => 13
+    text "  Bilangan item <b>#{@disposal.quantity}</b> dipindahkan / hadiah kepada <b>#{@disposal.receiver_name}</b> ", :align => :left, :width => 200, :size => 10, :indent_paragraphs => 13,:inline_format => true
     text  "(Surat Akuan Terima Disertakan)", :align => :left, :width => 200, :size => 10, :indent_paragraphs => 13
     
     move_down 10
     text "2.  Aset berikut telah dilupuskan secara jual.", :align => :left, :width => 200, :size => 10
-    text "Bilangan item #{@disposal.quantity}  No. Resit  #{@disposal.documentation_no} ", :align => :left, :width => 200, :size => 10, :indent_paragraphs => 13
+    text "Bilangan item </b>#{@disposal.quantity}</b>  No. Resit  <b>#{@disposal.documentation_no}</b> ", :align => :left, :width => 200, :size => 10, :indent_paragraphs => 13, :inline_format => true
     text "     (Salinan Resit Disertakan)", :align => :left, :width => 200, :size => 10, :indent_paragraphs => 13
     move_down 10
     text "3.  Aset berikut telah dilupuskan secara musnah.", :align => :left, :width => 200, :size => 10
-    text "Bilangan item : #{@disposal.quantity}", :align => :left, :width => 200, :size => 10, :indent_paragraphs => 13
-    text  "Cara dimusnahkan : #{@disposal.discard_options}", :align => :left, :width => 200, :size => 10, :indent_paragraphs => 13
+    text "Bilangan item : <b>#{@disposal.quantity}</b>", :align => :left, :width => 200, :size => 10, :indent_paragraphs => 13,:inline_format => true
+    text  "Cara dimusnahkan : <b>#{@disposal.discard_options}</b>", :align => :left, :width => 200, :size => 10, :indent_paragraphs => 13,:inline_format => true
     text "(Sijil Menyaksikan Pemusnahan disertakan)", :align => :left, :width => 200, :size => 10, :indent_paragraphs => 13
     
     move_down 10
     text "4.  Aset berikut telah dilupuskan melalui kaedah-kaedah lain.", :align => :left, :width => 200, :size => 10
-    text "Bilangan item : #{@disposal.quantity}", :align => :left, :width => 200, :size => 10, :indent_paragraphs => 13
-    text "Kaedah pelupusan : #{@disposal.type_others_desc}", :align => :left, :width => 200, :size => 10, :indent_paragraphs => 13
+    text "Bilangan item : <b>#{@disposal.quantity}</b>", :align => :left, :width => 200, :size => 10, :indent_paragraphs => 13, :inline_format => true
+    text "Kaedah pelupusan : <b>#{@disposal.type_others_desc}</b>", :align => :left, :width => 200, :size => 10, :indent_paragraphs => 13, :inline_format => true
     text "(Dokumen berkaitan disertakan)", :align => :left, :width => 200, :size => 10, :indent_paragraphs => 13
     move_down 10
     text "5.  Aset berikut telah dimasukkan ke dalam stok.", :align => :left, :width => 200, :size => 10
-    text "Bilangan item : #{@disposal.quantity}", :align => :left, :width => 200, :size => 10, :indent_paragraphs => 13
+    text "Bilangan item : <b>#{@disposal.quantity}</b>", :align => :left, :width => 200, :size => 10, :indent_paragraphs => 13
     text "Salinan Kad Kawalan Stok Disertakan)", :align => :left, :width => 200, :size => 10, :indent_paragraphs => 13
     move_down 40
     table1
