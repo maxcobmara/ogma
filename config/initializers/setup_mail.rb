4c1960159bd5ac7f1ee7a70fb3388491e3851fb0
Yaacob Noor <maxcobmara@gmail.com>
2014-08-13 00:13:55 +0800

    update email address for library

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "gmail.com",
-  :user_name            => "icms.kskb.jb",
-  :password             => "Maslinda",
+  :address              => "smtp.mail.yahoo.com",
+  :port                 => 465,
+  :domain               => "yahoo.com.com",
+  :user_name            => "pustakabistari_kskbjb@yahoo.com",
+  :password             => "kskbjb8173",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
