1a4c8c3fad121473bb75c928f905d8bba4d883a8
Nurhashimah <nurhashimah77@gmail.com>
2014-08-18 04:43:03 +0800

    Library Resources - Import Excel (Notice/msg pending)

--- config/routes.rb
+++ config/routes.rb
@@ -167,7 +167,7 @@ Ogma::Application.routes.draw do
     end
   end
   
-  match '/public/excel_format/books_import.xls', to: 'library/books#download_excel_format', via: 'get', target: '_self'
+  match '/public/excel_format/book_import.xls', to: 'library/books#download_excel_format', via: 'get', target: '_self'
 
   namespace :exam do
     resources :examquestions do
