89f7571788cba39877530e7cf122a2fb2f76f914
Yaacob Noor <maxcobmara@gmail.com>
2014-08-24 03:39:28 +0800

    fix for yahoo smtp settings

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,9 @@
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.mail.yahoo.com",
-  :port                 => 465,
-  :domain               => "yahoo.com.com",
+  :port                 => 587,
+  :domain               => "yahoo.com",
   :user_name            => "pustakabistari_kskbjb@yahoo.com",
   :password             => "kskbjb8173",
-  :authentication       => "plain",
+  :authentication       => :plain,
   :enable_starttls_auto => true
 }
