a1b0fd53afbbbb5d3f97391d6f9d2ce47fde4a5b
Yaacob Noor <maxcobmara@gmail.com>
2014-09-01 01:20:05 +0800

    staff qualifications show tab

--- app/models/employgrade.rb
+++ app/models/employgrade.rb
@@ -7,12 +7,12 @@ class Employgrade < ActiveRecord::Base
   
   validates_uniqueness_of :name, :scope => :group_id
   
-  def booboo
+  def grade_group
     (Employgrade::GROUP.find_all{|disp, value| value == group_id}).map {|disp, value| disp}
   end
   
   def name_and_group
-    "#{name}  (#{booboo})"
+    "#{name}  (#{grade_group[0]})"
   end
   
   def gred_no
