ef365a228defa2b8e175a7f51a1ff1fb039d525b
Azim Adnan <darkdistunnel@gmail.com>
2014-09-18 11:55:12 +0800

    fix rspec and appraisal index

--- app/controllers/staff/staff_appraisals_controller.rb
+++ app/controllers/staff/staff_appraisals_controller.rb
@@ -1,13 +1,15 @@
 class Staff::StaffAppraisalsController < ApplicationController
   def index
     @search = StaffAppraisal.search(params[:q])
+    @staff_appraisals = @search.result
+    @staff_appraisals = @staff_appraisals.page(params[:page]||1)
   end
   
   def show
   end
   
   def new
-  @staff_appraisal = StaffAppraisal.new
+  @staff_appraisals = StaffAppraisal.new
   end
   
   def edit
