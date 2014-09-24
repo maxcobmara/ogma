f0203fdce4baf7d2e6574d4a7031f8a226c06e66
irshad89 <isyaduddin@gmail.com>
2014-09-24 12:25:37 +0800

    leaveforstaff application

--- app/controllers/staff/leaveforstaffs_controller.rb
+++ app/controllers/staff/leaveforstaffs_controller.rb
@@ -2,10 +2,20 @@ class Staff::LeaveforstaffsController < ApplicationController
   before_action :set_leaveforstaff, only: [:show, :edit, :update, :destroy]
   
   def index
- @leaveforstaffs = Leaveforstaff.all
-    
+ @leaveforstaffs = Leaveforstaff.all    
+  end
+  def show
   end
   
+  def new
+    @leaveforstaffs = Leaveforstaff.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.xml  { render :xml => @leaveforstaffs }
+  end
+end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_leaveforstaff
