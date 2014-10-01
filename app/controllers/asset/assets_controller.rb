c056ff5b0bd69f517584787014999e2ee4b5aa3c
irshad89 <isyaduddin@gmail.com>
2014-10-01 18:06:54 +0800

    fix

--- app/controllers/asset/assets_controller.rb
+++ app/controllers/asset/assets_controller.rb
@@ -73,7 +73,7 @@ class Asset::AssetsController < ApplicationController
   end
   
   def kewpa4
-    if params[:search]
+    unless params[:search] == '0'
       @assets = Asset.where('substring(assetcode, 18, 2 ) =? AND assettype =?', "#{params[:search]}", 1)
       respond_to do |format|
         format.pdf do
@@ -84,7 +84,7 @@ class Asset::AssetsController < ApplicationController
                  end
              end
         else
-    @assets = Asset.find(assettype: 1)
+    @assets = Asset.where(assettype: 1)
     respond_to do |format|
       format.pdf do
         pdf = Kewpa4Pdf.new(@assets, view_context)
@@ -97,7 +97,7 @@ class Asset::AssetsController < ApplicationController
 end  
   
   def kewpa5
-    if params[:search]
+    unless params[:search] == '0'
       @assets = Asset.where('substring(assetcode, 18, 2 ) =? AND assettype =?', "#{params[:search]}", 2)
       respond_to do |format|
         format.pdf do
@@ -108,7 +108,7 @@ end
         end
       end
     else
-    @assets = Asset.find(assettype: 2).order(assetcode: :asc)
+    @assets = Asset.where(assettype: 2)
     respond_to do |format|
       format.pdf do
         pdf = Kewpa5Pdf.new(@assets, view_context)
--- app/pdfs/kewpa6_pdf.rb
+++ app/pdfs/kewpa6_pdf.rb
@@ -1,15 +1,16 @@
 class Kewpa6Pdf < Prawn::Document
   def initialize(asset, view)
     super({top_margin: 50, page_size: 'A4', page_layout: :portrait })
-    @assets = asset
+    @asset = asset
     @view = view
     font "Times-Roman"
     text "KEW.PA-6", :align => :right, :size => 16, :style => :bold
     move_down 15
     text "DAFTAR PERGERAKAN HARTA MODAL DAN INVENTORI", :align => :center, :size => 14, :style => :bold
-    asset_list
+    move_down 15
     table1
     table2
+    asset_list
    
   end
   
@@ -19,8 +20,9 @@ class Kewpa6Pdf < Prawn::Document
              ["No. Siri Pembuat:", "#{@asset.serialno}", "Pegawai Pengeluar",""],
              ["No. Siri Pendaftaran:", "#{@asset.assetcode}", "","Catatan"]]
              
-    table(data, :column_widths => [100, 100, 100,100]) do
-          row(0).background_color = 'FFE34D'
+    table(data, :column_widths => [120, 120, 200,80], :cell_style => { :size => 9}) do
+          row(3).column(3).align = :center
+          row(2).column(2).align = :center
           row(0).column(2).borders = [:left, :right, :top]
           row(0).column(3).borders = [:left, :right, :top]
           row(1).column(2).borders = [:left, :right]
@@ -28,26 +30,26 @@ class Kewpa6Pdf < Prawn::Document
           row(2).column(2).borders = [:left, :right]
           row(2).column(3).borders = [:left, :right]
           row(3).column(2).borders = [:left, :right, :bottom]
-          row(3).column(3).borders = [:left, :right, :bottom]
+          row(3).column(3).borders = [:left, :right]
         end
     
   end
   def table2
-    data = [["", "", "Tarikh", "","Ketika Dikeluarkan", "Ketika Dipulangkan", ""]]
+    data = [["", "Nama", "Tarikh", "Tandatangan","Ketika Dikeluarkan", "Ketika Dipulangkan", ""]]
              
-    table(data,:column_widths => [50, 100, 100,100]) do
+    table(data,:column_widths => [30, 80, 80,50,100,100,80 ], :cell_style => { :size => 9}) do
       row(0).column(2).borders = [:left, :right, :bottom]
-      row(0).column(3).borders = [:left, :right]
-      row(0).background_color = 'FFE34D'
+      row(0).column(0).borders = [:left, :right]
+      row(0).column(1).borders = [:left, :right]
+      row(0).column(6).borders = [:left, :right]
+      row(0).align = :center
     end
     
   end
   def asset_list
-    move_down 15
-    
-    table line_item_rows do
+
+    table(line_item_rows,:column_widths => [30, 80, 40,40,50,50,50,50,50,80 ], :cell_style => { :size => 9}) do
       
-      row(0).background_color = 'FFE34D'
       row(0).align = :center
       row(0).column(0).borders = [:left, :right, :bottom]
       row(0).column(1).borders = [:left, :right, :bottom]
@@ -60,7 +62,7 @@ class Kewpa6Pdf < Prawn::Document
   
   def line_item_rows
     counter = counter || 0
-    header = [[ 'Bil', 'Nama Peminjam', 'Dikeluarkan', 'Jangka Dipulangkan', 'Tandatangan Peminjam', 'Tandatangan', 'Tarikh', 'Tandatangan', 'Tarikh','']]
+    header = [[ 'Bil', 'Peminjam', 'Dikeluarkan', 'Jangka Dipulangkan', ' Peminjam', 'Tandatangan', 'Tarikh', 'Tandatangan', 'Tarikh','']]
     header +
       @asset.asset_loans.map do |asset_loan|
       ["#{counter += 1}", "#{asset_loan.staff_id}", "#{asset_loan.loaned_on.try(:strftime, "%d/%m/%y")}", " #{asset_loan.expected_on.try(:strftime, "%d/%m/%y")}", "","#{asset_loan.loaned_by}", 
