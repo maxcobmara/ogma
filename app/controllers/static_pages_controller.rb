2aca1fa60031f1c9b635f988e7dd1d993d5cac4d
Yaacob Noor <maxcobmara@gmail.com>
2014-10-11 15:52:32 +0800

    bulletins on home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,17 +1,17 @@
 class StaticPagesController < ApplicationController
   def home
+   @bulletins = Bulletin.order(publishdt: :desc).limit(10)
   end
 
   def help
   end
-  
+
   def about
   end
-  
+
   def contact
   end
-  
+
   def dashboard
-    @bulletins = Bulletin.order(publishdt: :desc).limit(10)
   end
 end
