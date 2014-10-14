efac4ea03e0130cc58c30ee7633e0b2b7c7aac48
irshad89 <isyaduddin@gmail.com>
2014-10-14 17:47:11 +0800

    fix staff leave list

--- app/controllers/staff/leaveforstaffs_controller.rb
+++ app/controllers/staff/leaveforstaffs_controller.rb
@@ -5,10 +5,31 @@ class Staff::LeaveforstaffsController < ApplicationController
  @leaveforstaffs = Leaveforstaff.all    
   end
   def show
+    @leaveforstaff = Leaveforstaff.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml  { render :xml => @leaveforstaff }
+    end
   end
   
   def create
-  end
+      @leaveforstaffs = Leaveforstaff.new(leaveforstaff_params)
+
+      respond_to do |format|
+        if @leaveforstaffs.save
+          flash[:notice] = 'Leaveforstaff was successfully created.'
+          format.html { redirect_to(@leaveforstaffs) }
+          format.xml  { render :xml => @leaveforstaffs, :status => :created, :location => @leaveforstaffs }
+        else
+          format.html { render :action => "new" }
+          format.xml  { render :xml => @leaveforstaffs.errors, :status => :unprocessable_entity }
+        end
+      end
+    end
+
+    # PUT /leaveforstaffs/1
+    # PUT /leaveforstaffs/1.xml
   
   def new
     @leaveforstaffs = Leaveforstaff.new
@@ -22,7 +43,7 @@ end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_leaveforstaff
-      @leaveforstaff = Leaveforstaff.find(params[:id])
+      @leaveforstaffs = Leaveforstaff.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
