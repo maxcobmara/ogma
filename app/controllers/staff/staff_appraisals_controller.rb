270e961f2b3b4bdbaa3cbe2d9a77030c5d1831b2
Nurhashimah <nurhashimah77@gmail.com>
2014-11-03 00:03:05 +0800

    #446 - 1.1.3 (4) - Second Evaluating Officer (PPK) must able t.....[Staff Appraisal (Edit + Show) revised]

--- app/controllers/staff/staff_appraisals_controller.rb
+++ app/controllers/staff/staff_appraisals_controller.rb
@@ -83,8 +83,7 @@ class Staff::StaffAppraisalsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def staff_appraisal_params
       params.require(:staff_appraisal).permit(:staff_id, :evaluation_year, :eval1_by, :eval2_by, :is_skt_submit, :skt_submit_on, :is_skt_endorsed, :skt_endorsed_on, :skt_pyd_report, :is_skt_pyd_report_done, :skt_pyd_report_on, :skt_ppp_report, :is_skt_ppp_report_done, :skt_ppp_report_on, :is_submit_for_evaluation, :submit_for_evaluation_on, :g1_questions, :g2_questions, :g3_questions, :e1g1q1, :e1g1q2,:e1g1q3, :e1g1q4, :e1g1q5,:e1g1_total, :e1g1_percent,:e1g2q1, :e1g2q2, :e1g2q3, :e1g2q4, :e1g2_total, :e1g2_percent, :e1g3q1, :e1g3q2, :e1g3q3, :e1g3q4, :e1g3q5, :e1g3_total, :e1g3_percent,:e1g4,:e1g4_percent, :e1_total, :e1_years, :e1_months, :e1_performance, :e1_progress, :is_submit_e2, :submit_e2_on, :e2g1q1, :e2g1q2, :e2g1q3,:e2g1q4, :e2g1q5,:e2g1_total, :e2g1_percent, :e2g2q1, :e2g2q2,:e2g2q3, :e2g2q4, :e2g2_total, :e2g2_percent, :e2g3q1, :e2g3q2, :e2g3q3,:e2g3q4,:e2g3q5, :e2g3_total,:e2g3_percent, :e2g4, :e2g4_percent,:e2_total, :e2_years, :e2_months, :e2_performance, :evaluation_total, :is_complete, :is_completed_on, 
-      staff_appraisal_skts_attributes: [:id,:_destroy, :priority, :description, :half, :indicator, :acheivment, :progress, :notes, :is_dropped, :dropped_on, :drop_reasons],
-      evactivities_attributes: [:id,:_destroy, :evaldt, :evactivity, :actlevel, :actdt], trainneeds_attributes: [:id,:_destroy, :name, :reason, :confirmedby_id, :evaluation_id])
+      staff_appraisal_skts_attributes: [:id,:_destroy, :priority, :description, :half, :indicator, :acheivment, :progress, :notes, :is_dropped, :dropped_on, :drop_reasons], evactivities_attributes: [:id,:_destroy, :evaldt, :evactivity, :actlevel, :actdt], trainneeds_attributes: [:id,:_destroy, :name, :reason, :confirmedby_id, :evaluation_id])
     end
   
 end
