bd9fd2643b0a6cba0417b7bbe8c859c7a6e46d48
Nurhashimah <nurhashimah77@gmail.com>
2014-11-05 23:41:24 +0800

    #460 - create link entity form as a form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,19 @@ class UsersController < ApplicationController
    @entity = Staff.where(coemail: current_user.email).first || Student.where(semail: current_user.email).first
   end
  end
+ 
+ def update
+    @user = User.find(params[:id])
+    respond_to do |format|
+       if @user.update(user_params)
+         format.html { redirect_to "/dashboard", notice: 'User was successfully updated.' }
+         format.json { head :no_content }
+       else
+         format.html { render action: 'link' }
+         format.json { render json: @user.errors, status: :unprocessable_entity }
+       end
+     end
+ end
 
  private
    # Use callbacks to share common setup or constraints between actions.
@@ -15,7 +28,7 @@ class UsersController < ApplicationController
 
    # Never trust parameters from the scary internet, only allow the white list through.
    def user_params
-     params.require(:user).permit(:email)
+     params.require(:user).permit(:email, :userable_id, :userable_type)
    end
 
 end
