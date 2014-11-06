5593c42c27605e96df6ccc6e55001c1e0f91562f
Nurhashimah <nurhashimah77@gmail.com>
2014-11-06 17:25:12 +0800

    #487 - Roles List

--- config/routes.rb
+++ config/routes.rb
@@ -268,7 +268,8 @@ Ogma::Application.routes.draw do
    end
   end
   resources :logins
-
+  resources :roles
+  
   root  'static_pages#home'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
