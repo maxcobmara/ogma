c6ff345823d9eb063fc34b831ef96249d2ff3794
mustafakamal <mustafakamalsaedon@gmail.com>
2015-01-02 19:18:14 +0800

    #574 - Lesson Plan Report: Location editable

--- app/controllers/training/lesson_plans_controller.rb
+++ app/controllers/training/lesson_plans_controller.rb
@@ -77,7 +77,12 @@ class Training::LessonPlansController < ApplicationController
     current_user.roles.each do |x|
       @current_roles << x.name
     end 
-
+    newlocation = params[:new_location]
+    scheduleid = params[:lesson_plan][:schedule]
+    schedule = WeeklytimetableDetail.where(id:scheduleid).first
+    schedule.location_desc = newlocation
+    schedule.save!
+    
     respond_to do |format|
       if @lesson_plan.update(lesson_plan_params)
         format.html { redirect_to(training_lesson_plan_path(@lesson_plan), :notice => t('training.lesson_plan.title')+t('actions.updated')) }
@@ -143,7 +148,7 @@ class Training::LessonPlansController < ApplicationController
   
   # Never trust parameters from the scary internet, only allow the white list through.
   def lesson_plan_params
-    params.require(:lesson_plan).permit(:lecturer, :intake_id, :student_qty, :semester, :topic, :lecture_title, :lecture_date, :start_time, :end_time, :reference, :is_submitted, :submitted_on, :hod_approved, :hod_approved_on, :hod_rejected, :hod_rejected_on, :data, :prerequisites, :year, :reason, :prepared_by, :endorsed_by, :condition_isgood, :condition_isnotgood, :condition_desc, :training_aids, :summary, :total_absent, :report_submit, :report_submit_on, :report_endorsed, :report_endorsed_on, :report_summary, :schedule,  lessonplan_methodologies_attributes: [:id,:content,:lecturer_activity, :student_activity, :training_aids, :evaluation, :start_meth, :end_meth, :_destroy], lesson_plan_trainingnotes_attributes: [:id,:_destroy,:lesson_plan_id,:trainingnote_id], trainingnotes_attributes: [:id,:_destroy,:document,:timetable_id,:staff_id,:title] )
+    params.require(:lesson_plan).permit(:lecturer, :intake_id, :student_qty, :location, :semester, :topic, :lecture_title, :lecture_date, :start_time, :end_time, :reference, :is_submitted, :submitted_on, :hod_approved, :hod_approved_on, :hod_rejected, :hod_rejected_on, :data, :prerequisites, :year, :reason, :prepared_by, :endorsed_by, :condition_isgood, :condition_isnotgood, :condition_desc, :training_aids, :summary, :total_absent, :report_submit, :report_submit_on, :report_endorsed, :report_endorsed_on, :report_summary, :schedule,  lessonplan_methodologies_attributes: [:id,:content,:lecturer_activity, :student_activity, :training_aids, :evaluation, :start_meth, :end_meth, :_destroy], lesson_plan_trainingnotes_attributes: [:id,:_destroy,:lesson_plan_id,:trainingnote_id], trainingnotes_attributes: [:id,:_destroy,:document,:timetable_id,:staff_id,:title] )
   end
   
 end
