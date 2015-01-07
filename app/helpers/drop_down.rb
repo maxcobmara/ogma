1efe0daee448e0e0c6edc83f90117261bf64afe1
mustafakamal <mustafakamalsaedon@gmail.com>
2015-01-07 17:52:29 +0800

    #588 - StudentLeave:add guardian/parents phone

--- app/helpers/drop_down.rb
+++ app/helpers/drop_down.rb
@@ -617,5 +617,21 @@ WEIGHTAGE = [
 
 ]
 
+#Kin
+KTYPE = [
+        #  Displayed       stored in db
+        [ "Isteri",1 ],
+        [ "Suami",2 ],
+        [ "Ibu", 3 ],
+        [ "Bapa", 4 ],
+        [ "Anak", 5 ],
+        [ "Nenek", 9 ],
+        [ "Saudara Kandung", 11 ],
+        [ "Penjaga", 12 ],
+        [ "Bekas Isteri", 13 ],
+        [ "Bekas Suami", 14 ],
+        [ "Penjamin I", 98 ],
+        [ "Penjamin II", 99 ]
+ ]
    
 end
