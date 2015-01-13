460a2bc4dc50e68967b7bc938c1f7087d37d9d8d
Yaacob Noor <maxcobmara@gmail.com>
2015-01-14 00:29:35 +0800

    update dashboard events and set timezone

--- config/application.rb
+++ config/application.rb
@@ -19,13 +19,14 @@ module Ogma
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = "Kuala Lumpur"
+    config.active_record.default_timezone = :local
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-    
+
     config.generators do |g|
       g.factory_girl false
     end
