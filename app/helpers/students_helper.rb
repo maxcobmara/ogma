4ba62165f061877b22871476a49bfa9fd4817bef
Nurhashimah <nurhashimah77@gmail.com>
2015-03-26 16:07:36 +0800

    Student Info - minor clean-up

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -40,5 +40,157 @@ module StudentsHelper
     end
     "#{mnth_group}/#{intake_year}"
   end
+  
+  #for Export Excel -- start
+  
+ def display_matrixno
+   unless matrixno.nil?
+     a="#{matrixno}"
+   else
+     nil
+   end
+ end
+ 
+ def display_race
+   unless race2.nil? 
+     a="#{((Student::RACE.find_all{|disp, value| value == race2.to_i}).map {|disp, value| disp}).first}"
+   else
+     a=nil
+   end
+   a
+ end
+
+ def display_intake
+   "#{intake.to_date.strftime("%b %Y") }"
+ end
+
+ def display_regdate
+   "#{regdate.to_date.strftime("%d-%m-%Y") unless regdate.nil?}"
+ end
+ def display_gender
+  "#{((Student::GENDER.find_all{|disp, value| value == gender.to_s}).map {|disp, value| disp}).first}"
+ end
+
+ def display_enddate
+   "#{end_training.to_date.strftime("%d-%m-%Y") unless end_training.nil?}"
+ end
+
+ def display_birthdate
+   "#{sbirthdt.to_date.strftime("%d-%m-%Y") unless sbirthdt.nil?}"
+ end
+ 
+ def display_bloodtype
+   if bloodtype.nil? || bloodtype.blank?
+     a=nil
+   else
+     a="#{((Student::BLOOD_TYPE.find_all{|disp, value| value == bloodtype.to_s}).map {|disp, value| disp}).first}"
+   end
+   a
+ end
+
+ def display_address
+   if address.nil? || address.blank?
+     a=nil
+   else
+     a="#{address.gsub!(/\s/," ")}"
+   end
+   a
+ end
+ def display_marital
+   "#{((Student::MARITAL_STATUS.find_all{|disp, value| value == mrtlstatuscd.to_s}).map {|disp, value| disp}).first}"
+ end
+ def display_status
+   "#{((Student::STATUS.find_all{|disp, value| value==sstatus}).map {|disp, value| disp}).first}"
+ end
+ def display_sstatus_remark
+   if sstatus == "Repeat"
+     unless sstatus_remark.nil?
+       a="#{I18n.t('student.students.semester_repeated')+": "+ sstatus_remark}"
+     else
+       a=nil
+     end
+   else
+     unless sstatus_remark.nil? 
+       a="#{sstatus_remark}"
+     else
+       a=nil
+     end
+   end
+   a
+ end
+ def display_programme
+   unless course_id.nil?
+     a=course.try(:programme_list)
+   else
+     a=nil
+   end
+   a
+ end
+ def display_physical
+   unless physical.blank?
+     a="#{physical}"
+   else
+     a=nil
+   end
+   a
+ end
+ def display_allergy
+   unless allergy.blank?
+     a="#{allergy}"
+   else
+     a=nil
+   end
+   a
+ end
+ def display_disease
+   unless disease.blank?
+     a="#{disease}"
+   else
+     a=nil
+   end
+   a
+ end
+ def display_offer_letter
+   unless offer_letter_serial.blank?
+     a="#{offer_letter_serial.to_s}"
+   else
+     a=nil
+   end
+   a
+ end
+ def display_semail
+   unless semail.blank?
+     a="#{semail}"
+   else
+     a=nil
+   end
+   a
+ end
+ def display_medication
+   unless medication.blank?
+     a="#{medication}"
+   else
+     a=nil
+   end
+   a
+ end
+ def display_medicalremarks
+   unless remarks.blank?
+     a="#{remarks}"
+   else
+     a=nil
+   end
+   a
+ end
+ def display_courseremarks
+   if course_remarks.blank? || course_remarks.nil?
+     a=nil
+   else
+     a="#{course_remarks}"
+   end
+   a
+ end
+  
+  #for Export Excel -- end
 
 end
