6a9377d3174d51c75e4743d40ccacd292a40f0c7
maxcobmara <maxcobmara@gmail.com>
2015-11-28 19:23:37 +0800

    fix show for exam templates

--- app/models/exam_template.rb
+++ app/models/exam_template.rb
@@ -1,6 +1,8 @@
 class ExamTemplate < ActiveRecord::Base
   serialize :data, Hash
 
+  belongs_to  :creator,       :class_name => 'User',   :foreign_key => 'created_by'
+
   def question_count=(value)
     data[:question_count] = value
   end
