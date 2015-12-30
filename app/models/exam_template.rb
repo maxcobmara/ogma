8c2edbe09c05fdb01f3d2c6425c8a06915e160d9
Nurhashimah <nurhashimah77@gmail.com>
2015-12-30 14:30:39 +0800

    Exam Template revised - (inc compulsory:1 question type c/w count) & full marks added, Exam & Exammark - full marks (rely on template n use)

--- app/models/exam_template.rb
+++ app/models/exam_template.rb
@@ -3,6 +3,9 @@ class ExamTemplate < ActiveRecord::Base
 
   belongs_to  :creator,       :class_name => 'User',   :foreign_key => 'created_by'
   has_many :exams
+  
+  validates_presence_of :name
+  validate :count_is_compulsory
 
   before_destroy :valid_for_removal
 
@@ -28,11 +31,13 @@ class ExamTemplate < ActiveRecord::Base
   def template_in_use
     in_string="("
     question_count.each do |k, v|
-      if v['count']!='' || v['weight']!=''
+      #if v['count']!='' || v['weight']!=''
+      if !v['count'].blank?  ||  !v['weight'].blank? ||  !v['full_marks'].blank?  
         in_string += k.upcase+" : "
-        in_string += v['count'] if v['count']!=''
-        in_string += "/"+ v['weight']+"%" if v["weight"]!=''
-        in_string+=", "
+        in_string += v['count'] unless v['count'].blank?                                   #if v['count']!=''
+        in_string += "/"+ v['weight']+"%" unless v['weight'].blank?                  #if v["weight"]!=''
+        in_string += "/"+ v['full_marks'] unless v['full_marks'].blank?
+        in_string +=", "
       end
     end
     in_string.gsub(/, $/,"")+")"
@@ -40,8 +45,9 @@ class ExamTemplate < ActiveRecord::Base
 
   def total_in_weight
     total=0
-    question_count.each{|k,v|total=v['weight'].to_f if k=="mcq"}
-    question_count.each{|k,v|total+=v['weight'].to_f if k!="mcq"}
+    question_count.each{|k,v|total+=v['weight'].to_f}
+    #question_count.each{|k,v|total=v['weight'].to_f if k=="mcq"}
+    #question_count.each{|k,v|total+=v['weight'].to_f if k!="mcq"}
     total
   end
   
@@ -66,6 +72,32 @@ class ExamTemplate < ActiveRecord::Base
       end
     end
   end
+  
+  #####use in exams/_show_exam 
+  def template_full_marks
+    sum=0
+    question_count.each do |k, v|
+      unless v['count'].blank?
+        qty=(v['count']).to_i
+        if !v["full_marks"].nil? && !v["full_marks"].blank?
+          sum1=v["full_marks"].to_f
+        else
+          if k=="mcq"
+            sum1=qty*1 
+          elsif k=="seq" || k=="ospe"
+            sum1=qty*10
+          elsif k=="meq"
+            sum1=qty*20
+          else
+            sum1=qty #default to 1 first
+          end
+        end
+        sum+=sum1
+      end #endfor unless v['count']
+    end #endfor question_count loop
+    sum
+  end
+  #####
 
   def valid_for_removal
     if Exam.where(topic_id: id).count > 0
@@ -74,5 +106,16 @@ class ExamTemplate < ActiveRecord::Base
       return true
     end
   end
+  
+  private 
+    def count_is_compulsory
+      q_withcount=0
+      question_count.each do |k, v|
+        q_withcount+=1 unless v['count'].blank?
+      end
+      if q_withcount==0
+        errors.add(:base, I18n.t('exam.exam_template.min_onetype_withcount'))
+      end
+    end
 
 end
