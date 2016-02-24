b9e5f55bfcdd431261e0ddeac2dd604f6ef81753
Nurhashimah <nurhashimah77@gmail.com>
2016-02-24 13:09:42 +0800

    Mailbox minor fixes + Translation fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,4 +78,7 @@ Ogma::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  #devise config
+  config.action_mailer.default_url_options = { :host => 'localhost:3003' }
 end
