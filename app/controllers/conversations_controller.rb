2cb4c5a38e7799e6b03992d8f9e019fcd558d137
Nurhashimah <nurhashimah77@gmail.com>
2016-02-26 17:42:30 +0800

    messaging - fix show(replied/not yet replied) & fix disp 'Re:'(replied msg only)

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -7,9 +7,14 @@ class ConversationsController < ApplicationController
   
   def create
     recipients = User.where(id: conversation_params[:recipients])
-    conversation = current_user.send_message(recipients, conversation_params[:body], conversation_params[:subject]).conversation
-    flash[:success] = "Your message was successfully sent!"
-    redirect_to conversation_path(conversation)
+    if conversation_params[:recipients].count > 1
+      conversation = current_user.send_message(recipients, conversation_params[:body], conversation_params[:subject]).conversation
+      flash[:success] =(t 'conversation.message_sent')+conversation_params[:recipients].count.to_s
+      redirect_to conversation_path(conversation)
+    else
+      flash[:error] =t 'conversation.select_recipient'
+      redirect_to(:back)
+    end
   end
 
   def show
