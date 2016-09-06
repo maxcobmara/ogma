d2e33d5e04188209a86dc4d702fa9b9c2868cd8e
Nurhashimah <nurhashimah77@gmail.com>
2016-09-06 22:53:13 +0800

    minor fixes

--- lib/subdomain.rb
+++ lib/subdomain.rb
@@ -1,7 +1,7 @@
 class Subdomain
    def matches?(request)
-     request.subdomain.present? && request.subdomain !="www" #works fine at local
+     #request.subdomain.present? && request.subdomain !="www" #works fine at local
      #request.subdomain(2).present? && request.subdomain(2) !="www"  #required at remote svr
-     request.subdomain(3).present? && request.subdomain(3) !="www"
+     request.subdomain(4).present? && request.subdomain(4) !="www"
    end
 end
