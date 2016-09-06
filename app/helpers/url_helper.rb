296692e8e63ed491ee8443e9a5d9fdda69c6d9ac
Nurhashimah <nurhashimah77@gmail.com>
2016-09-06 23:32:12 +0800

    works locally

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -2,9 +2,9 @@ module UrlHelper
   def with_subdomain(subdomain)
     subdomain = (subdomain || "")
     subdomain+="." unless subdomain.empty?
-    #[subdomain, request.domain, request.port_string].join
+    [subdomain, request.domain, request.port_string].join
      #[subdomain, request.domain(2), request.port_string].join
-    [subdomain, request.domain(4), request.port_string].join
+    #[subdomain, request.domain(4), request.port_string].join
   end
   
   def url_for(options = nil)
