d867838d8dda7d6461674a02307a055d7175d34e
Nurhashimah <nurhashimah77@gmail.com>
2016-09-07 13:25:09 +0800

    Set Subdomain size to 2

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -2,9 +2,8 @@ module UrlHelper
   def with_subdomain(subdomain)
     subdomain = (subdomain || "")
     subdomain+="." unless subdomain.empty?
-    [subdomain, request.domain, request.port_string].join
-     #[subdomain, request.domain(2), request.port_string].join
-    #[subdomain, request.domain(4), request.port_string].join
+    #[subdomain, request.domain, request.port_string].join #works fine at local, http://lvh.me --> http://amsas.lvh.me, http://kskbjb.lvh.me
+    [subdomain, request.domain(2), request.port_string].join #use this for production mode @ remote svr, http://teknikpadu.com.my --> http://amsas.teknikpadu.com.my, http://kskbjb.teknikpadu.com.my
   end
   
   def url_for(options = nil)
--- lib/subdomain.rb
+++ lib/subdomain.rb
@@ -1,7 +1,6 @@
 class Subdomain
    def matches?(request)
-     request.subdomain.present? && request.subdomain !="www" #works fine at local
-     #request.subdomain(2).present? && request.subdomain(2) !="www"  #required at remote svr
-     #request.subdomain(4).present? && request.subdomain(4) !="www"
+     #request.subdomain.present? && request.subdomain !="www" #works fine at local, http://lvh.me --> http://amsas.lvh.me, http://kskbjb.lvh.me
+     request.subdomain(2).present? && request.subdomain(2) !="www"  #use this for production mode @ remote svr, http://teknikpadu.com.my --> http://amsas.teknikpadu.com.my, http://kskbjb.teknikpadu.com.my
    end
 end
