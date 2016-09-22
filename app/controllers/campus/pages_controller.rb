7dc87ac36fdadb36f4ed9f5289865e63aa42c25f
Nurhashimah <nurhashimah77@gmail.com>
2016-09-22 17:45:56 +0800

    Pages revised : library rules part 1

---
+++ app/controllers/campus/pages_controller.rb
@@ -0,0 +1,64 @@
+class Campus::PagesController < ApplicationController
+  #filter_resource_access
+  filter_access_to :index, :new, :create, :attribute_check => false
+  filter_access_to :show, :flexipage, :edit, :update, :destroy, :attribute_check => true
+  before_action :set_page, only: [:show, :edit, :update, :destroy,  :flexipage]
+  
+  def index
+    @search=Page.search(params[:q])
+    @pages= @search.result.order(position: :asc)
+    #@pages= @pages.page(params[:page]||1)  # TODO - same parameter name in use  --> :page
+  end
+
+  def new
+    @page = Page.new
+  end
+
+  def show
+  end
+  
+  def flexipage
+  end
+
+  def edit
+  end
+
+  def create 
+    @page = Page.new(page_params)
+
+    respond_to do |format|
+      if @page.save
+        flash[:notice] = (t 'campus.pages.title')+(t 'actions.created')
+        format.html { redirect_to campus_pages_path}
+        format.json { render action: 'show', status: :created, location: @page }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @page.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @page.update(page_params)
+        flash[:notice] = (t 'campus.pages.title')+(t 'actions.updated')
+        format.html { redirect_to campus_page_path(@page)}
+        format.json { render action: 'show', status: :created, location: @page }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @page.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+	private
+	  # Use callbacks to share common setup or constraints between actions.
+	  def set_page
+	    @page = Page.find(params[:id])
+	  end
+
+	  # Never trust parameters from the scary internet, only allow the white list through.
+	  def page_params
+	    params.require(:page).permit(:name, :title, :body, :body2, :body3, :admin, :parent_id, :navlabel, :position, :redirect, :action_name, :controller_name, :college_id, {:data => []})
+	  end
+end
--- app/models/page.rb
+++ app/models/page.rb
@@ -2,4 +2,17 @@ class Page < ActiveRecord::Base
    has_many :subpages, :class_name => 'Page'
    belongs_to :parent, :class_name => 'Page', foreign_key: 'parent_id'
    belongs_to :college, foreign_key: 'college_id'
+   
+   before_save :set_body_val
+   
+   attr_accessor :body2, :body3
+   
+   def set_body_val
+     if body2!=body
+       self.body=body2
+     elsif body3!=body
+       self.body=body3
+     end
+   end
+   
 end
