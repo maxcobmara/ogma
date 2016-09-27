5809f366c2a7a4e8b4fe1f88edb2cd471a1b5301
Nurhashimah <nurhashimah77@gmail.com>
2016-09-27 23:57:40 +0800

    Auth rules added-Booking Facilities

--- app/models/bookingfacility.rb
+++ app/models/bookingfacility.rb
@@ -2,6 +2,7 @@ class Bookingfacility < ActiveRecord::Base
   belongs_to :booked_facility, class_name: 'Location',  foreign_key: 'location_id'
   belongs_to :booking_staff, class_name: 'Staff', foreign_key: 'staff_id'
   belongs_to :approving_staff, class_name: 'Staff', foreign_key: 'approver_id'
+  belongs_to :facility_officer, class_name: 'Staff', foreign_key: 'approver_id2'
 
   validates :location_id, :staff_id, :request_date, :start_date, :end_date, :total_participant, :purpose, presence: true  
   validates :total_participant, numericality: true
