2ff89b869e2e7c666fc6d0c091d55bf602e18260
Nurhashimah <nurhashimah77@gmail.com>
2016-10-31 00:29:28 +0800

    Booking Facility - Notification added & Auth rules revision

--- app/models/bookingfacility.rb
+++ app/models/bookingfacility.rb
@@ -27,5 +27,20 @@ class Bookingfacility < ActiveRecord::Base
       errors.add(:base, I18n.t('campus.bookingfacilities.begin_before_ends')) if end_date < start_date || start_date < DateTime.now
     end
   end
+  
+  def self.search2(search)
+    if search
+      where('staff_id=? OR approver_id=? OR approver_id2=?', search, search, search)
+    end
+  end
+  
+  def location_admin(staff_id)
+    if User.where(userable_id: staff_id).first.roles.pluck(:authname).include?('facilities_administrator')
+      location_ids=Location.where(staffadmin_id: staff_id).pluck(:id)
+    else
+      location_ids=[]
+    end
+    location_ids
+  end
 
 end
