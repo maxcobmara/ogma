b616e64c5355ad99c738050222bdedb538509379
Nurhashimah <nurhashimah77@gmail.com>
2016-11-17 19:18:14 +0800

    misc fix

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -9,8 +9,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "gmail.com",
-  :user_name => ENV["GMAIL_USERNAME"], #College.where(code: 'tpsb').first.library_email,   # "applications@teknikpadu.com",
-  :password => ENV["GMAIL_PASSWORD"], #College.where(code: 'tpsb').first.library_pwd,       #"ThisismyPassword",
+  :user_name => ENV["GMAIL_USERNAME"], 
+  :password => ENV["GMAIL_PASSWORD"],
   :authentication       => :plain,
   :enable_starttls_auto => true
 }
