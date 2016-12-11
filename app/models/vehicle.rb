0e0dd77c090f8d15da60fd4995a5f7a00061d32c
Nurhashimah <nurhashimah77@gmail.com>
2016-12-11 19:59:58 +0800

    Staff dependencies revision

--- app/models/vehicle.rb
+++ app/models/vehicle.rb
@@ -7,8 +7,8 @@ class Vehicle < ActiveRecord::Base
   
   private
   
-    def  travel_own_car_exist
-      travellings_own_car = TravelRequest.find(:all, :conditions => ['staff_id=? and own_car=?', staff_id, true]).count
+    def travel_own_car_exist
+      travellings_own_car = TravelRequest.where(staff_id: staff_id).where(own_car: true).count
       if travellings_own_car > 0 
         return false
         #errors.add(:base, I18n.t('vehicles.is_not_removable'))
