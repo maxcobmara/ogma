23b0a25c0e08ca3836be3a24f4a358d6b8623939
Nurhashimah <nurhashimah77@gmail.com>
2016-12-14 19:18:36 +0800

    Position & User rev

--- app/helpers/positions_helper.rb
+++ app/helpers/positions_helper.rb
@@ -6,21 +6,25 @@ module PositionsHelper
         #Category.siblings_of(params[:id]).order_by(name: :asc)
         pa=Position.where('name=? or tasks_main ILIKE(?) or tasks_other ILIKE(?)', 'Setiausaha Pejabat',  '%PA Pengarah%', '%PA Pengarah%').first
         positions.map do |position, sub_position|
-           #sub_position.sort -> will just sort by ID
-           subp=Position.children_of(position).order(combo_code: :asc)
-           if position.is_root?
-             #pa=Position.where('name=? or tasks_main ILIKE(?) or tasks_other ILIKE(?)', 'Setiausaha Pejabat',  '%PA Pengarah%', '%PA Pengarah%').first
-             if pa 
-               #content_tag(:li, link_to(position_details(position).html_safe, staff_position_path(position))+content_tag(:adjunct, link_to(position_details2(pa).html_safe, staff_position_path(pa)))+ nested_groups(subp))
-	       content_tag(:li, link_to(position_details(position).html_safe, staff_position_path(position))+content_tag(:adjunct, link_to(position_details2(pa).html_safe, staff_position_path(pa)))+ nested_groups(subp))
+          if (position.name.include?('Jangan Delete Dulu')==false) || is_developer?
+             #####
+             #sub_position.sort -> will just sort by ID
+             subp=Position.children_of(position).order(combo_code: :asc)
+             if position.is_root?
+               #pa=Position.where('name=? or tasks_main ILIKE(?) or tasks_other ILIKE(?)', 'Setiausaha Pejabat',  '%PA Pengarah%', '%PA Pengarah%').first
+               if pa 
+                 #content_tag(:li, link_to(position_details(position).html_safe, staff_position_path(position))+content_tag(:adjunct, link_to(position_details2(pa).html_safe, staff_position_path(pa)))+ nested_groups(subp))
+	         content_tag(:li, link_to(position_details(position).html_safe, staff_position_path(position))+content_tag(:adjunct, link_to(position_details2(pa).html_safe, staff_position_path(pa)))+ nested_groups(subp))
+               else
+                 content_tag(:li, link_to(position_details(position).html_safe, staff_position_path(position)) + nested_groups(subp))
+               end
              else
-               content_tag(:li, link_to(position_details(position).html_safe, staff_position_path(position)) + nested_groups(subp))
+	       if position!=pa
+                 content_tag(:li, link_to(position_details(position).html_safe, staff_position_path(position)) + nested_groups(subp)) 
+	       end
              end
-           else
-	     if position!=pa
-             content_tag(:li, link_to(position_details(position).html_safe, staff_position_path(position)) + nested_groups(subp)) 
-	     end
-           end
+             #####
+          end
         end.join.html_safe
      end  
   end
