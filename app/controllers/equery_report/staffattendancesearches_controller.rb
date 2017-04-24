2928a98c8969a972ed4d07814e015f58efabf98e
Nurhashimah <nurhashimah77@gmail.com>
2017-04-24 18:41:33 +0800

    Equery - Staffattendancesearh rev part 4(refactor)

--- app/controllers/equery_report/staffattendancesearches_controller.rb
+++ app/controllers/equery_report/staffattendancesearches_controller.rb
@@ -1,5 +1,5 @@
 class EqueryReport::StaffattendancesearchesController < ApplicationController
-  filter_access_to :all
+  filter_resource_access
   
   def new
     @searchstaffattendancetype = params[:searchattendancetype]
@@ -7,7 +7,6 @@ class EqueryReport::StaffattendancesearchesController < ApplicationController
   end
 
   def create
-    #@searchstaffattendancetype = params[:method]
     @staffattendancesearch = Staffattendancesearch.new(params[:staffattendancesearch])
     if !@staffattendancesearch.department.blank? && !@staffattendancesearch.thumb_id.blank? && !@staffattendancesearch.logged_at.blank?
       if @staffattendancesearch.save
@@ -19,23 +18,6 @@ class EqueryReport::StaffattendancesearchesController < ApplicationController
       flash[:notice] = t('equery.staffattendance.select_all_fields')
       redirect_to new_equery_report_staffattendancesearch_path(@staffattendancesearch,  :searchattendancetype =>1)
     end
-  end
-  
-  def view_staff
-    unless params[:department2].blank?
-      department_name = params[:department2]
-      thumb_ids=Staff.find(:all, :conditions => ['staff_shift_id is not null AND thumb_id is not null']).map(&:thumb_id)
-      @staff_list=Staff.find(:all, :joins => :position, :conditions => ['(unit=? OR tasks_main ILIKE(?)) and thumb_id IN(?) and positions.name!=?', department_name, "%#{department_name}%", thumb_ids,'ICMS Vendor Admin'], :order => 'name ASC')
-    end
-    render :partial => 'view_staff', :layout => false
-  end
-  
-  def view_monthyear
-    unless params[:thumbid].blank?
-      thumbid = params[:thumbid]
-      @staryear = StaffAttendance.find(:first, :conditions => ['thumb_id=?', thumbid], :order=>'logged_at ASC').logged_at.year
-    end
-    render :partial => 'view_monthyear', :layout => false
   end
  
   def show
