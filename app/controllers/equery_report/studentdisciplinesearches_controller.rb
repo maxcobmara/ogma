d00e519d97c5ca57a9ecd08d605b2fdaf7fafcf0
Nurhashimah <nurhashimah77@gmail.com>
2017-05-15 16:19:15 +0800

    Equery-Studentdisciplinesearch added

--- app/controllers/equery_report/studentdisciplinesearches_controller.rb
+++ app/controllers/equery_report/studentdisciplinesearches_controller.rb
@@ -8,40 +8,21 @@ class EqueryReport::StudentdisciplinesearchesController < ApplicationController
   end
 
   def create
-    #raise params.inspect
     @searchstudentdisciplinetype = params[:method]
     if (@searchstudentdisciplinetype == '1' || @searchstudentdisciplinetype == 1)
         @studentdisciplinesearch = Studentdisciplinesearch.new(params[:studentdisciplinesearch])
-#         @aa=params[:intake][:"(1i)"] 
-#         @bb=params[:intake][:"(2i)"]
-#         @cc=params[:intake][:"(3i)"]
-#         if @aa!='' && @bb!='' #&& @cc!=''
-#             @dadidu=@aa+'-'+@bb+'-'+'01' 
-#         else
-#             @dadidu=''
-#         end
-        #@studentdisciplinesearch.intake = params[:intake]#@dadidu
     end
-    if @studentdisciplinesearch.save
-      #flash[:notice] = "Successfully created studentdisciplinesearch."
-      redirect_to equery_report_studentdisciplinesearch_path(@studentdisciplinesearch)
-    else
-      render :action => 'new'
-    end
+      if @studentdisciplinesearch.save
+        redirect_to equery_report_studentdisciplinesearch_path(@studentdisciplinesearch)
+      else
+        render :action => 'new'
+      end
+   
   end
-  
-#   def view_intake
-#     @programme_id = params[:programmeid]
-#     a=StudentDisciplineCase.all.map(&:student_id)
-#     @intakes = Student.find(:all, :conditions => ['course_id=? and id IN(?)', @programme_id, a], :order => 'intake ASC', :select => 'distinct intake, course_id')
-#     
-#     render :partial => 'view_intake', :layout => false
-#   end
 
   def show
     @studentdisciplinesearch = Studentdisciplinesearch.find(params[:id])
     @studentdisciplines=@studentdisciplinesearch.studentdisciplines.page(params[:page]).per(10)
-    #render :layout => 'report'
   end
   
   private
