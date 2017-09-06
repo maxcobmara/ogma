94a69e7573c54062448989cb4eb83e4fb80923f9
Nurhashimah <nurhashimah77@gmail.com>
2017-09-06 17:58:23 +0800

    minor

--- app/models/position.rb
+++ app/models/position.rb
@@ -332,6 +332,7 @@ class Position < ActiveRecord::Base
   #use in Weeklytimetables_controller.rb (Index - retrieve Programme ID for Pos Basik/Pengkhususan/Diploma Lanjutan) - END
   
   #usage - views/staffattendancesearches/_form.html.haml
+  #usage - views/staff_attendances/attendance_report.html.haml
   def self.department_list(college_code)
     if college_code=='kskbjb'
       programme_names=['Diploma', 'Diploma Lanjutan', 'Pos Basik', 'Pengkhususan']
@@ -347,6 +348,7 @@ class Position < ActiveRecord::Base
   end
   
   #usage - views/staffattendancesearches/_form.html.haml
+  #usage - views/staff_attendances/attendance_report.html.haml
   def self.thumbids_per_department
     #6thSept2017 - match w SA ind/search
     initial_amsas_posts=[Position.where(name: 'Jangan Delete Dulu').first.id]+Position.where(name: 'Jangan Delete Dulu').first.descendant_ids
