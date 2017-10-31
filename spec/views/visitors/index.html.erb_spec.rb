09c27e6bcc7b7e6d0997453b2cda3a81dce12a0e
Nurhashimah <nurhashimah77@gmail.com>
2017-10-31 14:42:34 +0800

    Rspec:fixes - Visitor view page- Index rev

--- spec/views/visitors/index.html.erb_spec.rb
+++ spec/views/visitors/index.html.erb_spec.rb
@@ -35,7 +35,7 @@ RSpec.describe "campus/visitors/index", :type => :view do
     @college=FactoryGirl.create(:college)
     @address_book=FactoryGirl.create(:address_book)
     @visitor1=FactoryGirl.create(:visitor, name: "Visitor 1", icno: "123456789101", rank_id: @rank.id, title_id: @title.id, college_id: @college.id, corporate: true, address_book_id: @address_book.id, phoneno: "1234567891", hpno: "1234567890", email:  "visitor1@example.com", expertise: "My Expertise")
-    @visitor2=FactoryGirl.create(:visitor, name: "Visitor 1", icno: "123456789101", rank_id: @rank.id, title_id: @title.id, college_id: @college.id, corporate: true, address_book_id: @address_book.id, phoneno: "1234567891", hpno: "1234567890", email:  "visitor1@example.com", expertise: "My Expertise")
+    @visitor2=FactoryGirl.create(:visitor, name: "Visitor 1", icno: "123456789101", rank_id: @rank.id, title_id: @title.id, college_id: @college.id, corporate: false, department: "Department1", phoneno: "1234567891", hpno: "1234567890", email:  "visitor1@example.com", expertise: "My Expertise")
     
     @admin_user=FactoryGirl.create(:admin_user)
     sign_in(@admin_user)
@@ -46,8 +46,9 @@ RSpec.describe "campus/visitors/index", :type => :view do
   it "renders a list of visitors" do
 #     render
     render :template => 'campus/visitors/index', :layout => false
-   
+    assert_select "tr>td", :text => "#{formatted_mykad(@visitor1.icno)}".to_s, :count => 2
     assert_select "tr>td", :text => "#{@visitor1.visitor_with_title_rank}".to_s, :count => 2
-    assert_select "tr>td", :text => "#{@address_book.name}".to_s, :count => 2
+    assert_select "tr>td", :text => "#{@address_book.name}".to_s, :count => 1
+    expect(rendered).to match(/Department1/)
   end
 end
