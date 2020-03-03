cda98c47e7cc96e26d0a703d29c993af1039100c
Yaacob Noor <maxcobmara@gmail.com>
2020-03-03 15:23:15 +0800

    some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "Creating Academic Sessions"
+(2012..2020).each do | y |
+  AcademicSession.create!([
+    {semester: "1/#{y}", total_week: 26},
+    {semester: "2/#{y}", total_week: 26}
+  ])
+end
+
+puts "Not Creating Accessions"
+
+
+puts "Creating Address Book"
+60.times do
+  comp = Faker::Company.name
+  domain  = comp.parameterize.underscore
+  AddressBook.create!({
+    name: "#{Faker::Name.first_name} #{Faker::Name.last_name}",
+    phone: Faker::PhoneNumber.phone_number,
+    address: Faker::Address.street_address,
+    mail: "#{Faker::Name.first_name}@#{domain}.com",
+    web: "www.#{domain}.com",
+    fax: Faker::PhoneNumber.phone_number,
+    shortname: nil
+  })
+end
---
+++ resetdb.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+bundle exec rake db:drop
+bundle exec rake db:create
+bundle exec rake db:migrate
+bundle exec rake db:seed
