a0b3278dcc654694e38556eab38e919651bd8ab2
Yaacob Noor <maxcobmara@gmail.com>
2020-03-13 12:18:35 +0800

    b seeds

---
+++ db/seeds/Booleanchoice.rb
@@ -0,0 +1,7 @@
+Booleanchoice.create!([
+  {id: 1, examquestion_id: 84, item: "A", description: "aaaa", created_at: "2015-12-08 10:58:12", updated_at: "2015-12-08 10:58:12"},
+  {id: 2, examquestion_id: 84, item: "B", description: "bbbb", created_at: "2015-12-08 10:58:12", updated_at: "2015-12-08 10:58:12"},
+  {id: 3, examquestion_id: 84, item: "C", description: "cccc", created_at: "2015-12-08 10:58:12", updated_at: "2015-12-08 10:58:12"},
+  {id: 4, examquestion_id: 84, item: "D", description: "dddd", created_at: "2015-12-08 10:58:12", updated_at: "2015-12-08 10:58:12"},
+  {id: 5, examquestion_id: 84, item: "E", description: "eeee", created_at: "2015-12-08 10:58:12", updated_at: "2015-12-08 10:58:12"}
+])
