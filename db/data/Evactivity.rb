53bd07b3908818cc94a458e4f64c1636219b6b5a
Yaacob Noor <maxcobmara@gmail.com>
2020-03-18 12:31:32 +0800

    update data

---
+++ db/data/Evactivity.rb
@@ -0,0 +1,21 @@
+Evactivity.create!([
+  {id: 17, appraisal_id: 28, evaldt: nil, evactivity: "Perserta Larian Jogathon Hari Kebangsaan", actlevel: "4", actdt: "2012-10-12", created_at: "2012-11-01 07:04:47", updated_at: "2012-11-01 07:04:47"},
+  {id: 18, appraisal_id: 28, evaldt: nil, evactivity: "Ahli Jawatankuasa Surau AsSyifa KSKBJB", actlevel: "2", actdt: "2012-01-09", created_at: "2012-11-01 07:04:47", updated_at: "2012-11-01 07:04:47"},
+  {id: 21, appraisal_id: 3, evaldt: nil, evactivity: "Gotong royong", actlevel: "1", actdt: "2012-11-16", created_at: "2012-11-16 05:45:41", updated_at: "2012-11-16 05:45:41"},
+  {id: 22, appraisal_id: 8, evaldt: nil, evactivity: "Gotong Royong", actlevel: "1", actdt: "2010-11-28", created_at: "2012-11-28 11:06:09", updated_at: "2012-11-28 11:06:09"},
+  {id: 23, appraisal_id: 8, evaldt: nil, evactivity: "Program Sambutan Aidilfitri", actlevel: "2", actdt: "2010-06-24", created_at: "2012-11-28 11:06:09", updated_at: "2012-11-28 11:06:09"},
+  {id: 28, appraisal_id: 34, evaldt: nil, evactivity: "gotong royong", actlevel: "1", actdt: "2012-11-29", created_at: "2012-11-29 12:36:40", updated_at: "2012-11-29 12:36:40"},
+  {id: 29, appraisal_id: 34, evaldt: nil, evactivity: "aidilfitri", actlevel: "2", actdt: "2012-11-29", created_at: "2012-11-29 12:36:40", updated_at: "2012-11-29 12:36:40"},
+  {id: 30, appraisal_id: 35, evaldt: nil, evactivity: "gotong royong", actlevel: "1", actdt: "2012-11-29", created_at: "2012-11-29 13:00:41", updated_at: "2012-11-29 13:00:41"},
+  {id: 31, appraisal_id: 35, evaldt: nil, evactivity: "sambutan aidilfitri", actlevel: "2", actdt: "2012-11-29", created_at: "2012-11-29 13:00:41", updated_at: "2012-11-29 13:00:41"},
+  {id: 32, appraisal_id: 36, evaldt: nil, evactivity: "gotong royong", actlevel: "1", actdt: "2012-11-29", created_at: "2012-11-29 14:02:56", updated_at: "2012-11-29 14:02:56"},
+  {id: 33, appraisal_id: 36, evaldt: nil, evactivity: "sambutan aidilfitri", actlevel: "2", actdt: "2012-11-29", created_at: "2012-11-29 14:02:56", updated_at: "2012-11-29 14:02:56"},
+  {id: 34, appraisal_id: 37, evaldt: nil, evactivity: "gotong royong", actlevel: "1", actdt: "2012-11-29", created_at: "2012-11-29 14:32:30", updated_at: "2012-11-29 14:32:30"},
+  {id: 35, appraisal_id: 37, evaldt: nil, evactivity: "sambutan aidilfitri", actlevel: "2", actdt: "2012-11-29", created_at: "2012-11-29 14:32:30", updated_at: "2012-11-29 14:32:30"},
+  {id: 36, appraisal_id: 38, evaldt: nil, evactivity: "Juri pertandingan berbalas pantun", actlevel: "2", actdt: "2012-12-11", created_at: "2012-12-11 01:49:25", updated_at: "2012-12-11 01:49:25"},
+  {id: 37, appraisal_id: 38, evaldt: nil, evactivity: "Urusetia pesta kebudayaan", actlevel: "3", actdt: "2012-12-11", created_at: "2012-12-11 01:49:25", updated_at: "2012-12-11 01:49:25"},
+  {id: 38, appraisal_id: 57, evaldt: nil, evactivity: "Gotong royong", actlevel: "1", actdt: nil, created_at: "2015-06-10 04:47:22", updated_at: "2015-06-10 04:47:22"},
+  {id: 39, appraisal_id: 66, evaldt: nil, evactivity: "Gotong royong", actlevel: "1", actdt: nil, created_at: "2015-06-10 08:37:39", updated_at: "2015-06-10 08:37:39"},
+  {id: 40, appraisal_id: 67, evaldt: nil, evactivity: "Gotong royong", actlevel: "2", actdt: nil, created_at: "2015-06-10 09:08:16", updated_at: "2015-06-10 09:08:16"},
+  {id: 41, appraisal_id: 68, evaldt: nil, evactivity: "gotong royong", actlevel: "2", actdt: nil, created_at: "2015-06-14 07:57:17", updated_at: "2015-06-14 07:57:17"}
+])
